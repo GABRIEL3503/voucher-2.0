5fb330680c4dcabe6aeddcea7b7e1915d51d7d80
gabriel M <gabriel_jm_3@hotmail.com>
2023-09-08 02:07:50 -0300

    vaucher

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
+
+// Crear una nueva base de datos o abrir una existente
+const db = new sqlite3.Database('./voucher_system.db');
+
+// Crear tabla si no existe
+db.run(`CREATE TABLE IF NOT EXISTS vouchers (
+    id TEXT PRIMARY KEY,
+    message TEXT,
+    redeemed INTEGER
+)`);
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(express.static('public'));
+// Endpoint para crear un nuevo voucher
+app.post('/create', (req, res) => {
+  const { id, message } = req.body;
+  const query = 'INSERT INTO vouchers (id, message, redeemed) VALUES (?, ?, 0)';
+  db.run(query, [id, message], function(err) {
+    if (err) {
+      res.status(500).send('Error al crear el voucher');
+      return;
+    }
+    res.status(200).send('Voucher creado exitosamente');
+  });
+});
+
+// Endpoint para validar un voucher
+app.get('/validate/:id', (req, res) => {
+  const { id } = req.params;
+  const query = 'SELECT * FROM vouchers WHERE id = ? AND redeemed = 0';
+  db.get(query, [id], (err, row) => {
+    if (err || !row) {
+      res.status(404).send('Voucher no encontrado o ya canjeado');
+      return;
+    }
+    // Marcar como canjeado
+    db.run('UPDATE vouchers SET redeemed = 1 WHERE id = ?', [id], function(err) {
+      if (err) {
+        res.status(500).send('Error al actualizar el estado del voucher');
+        return;
+      }
+      res.status(200).send(`Voucher válido: ${row.message}`);
+    });
+  });
+});
+
+// Iniciar el servidor
+app.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000');
+});
+// Endpoint para validar un voucher
+app.get('/validate/:id', async (req, res) => {
+    const { id } = req.params;
+    const query = 'SELECT * FROM vouchers WHERE id = ? AND redeemed = 0';
+    db.get(query, [id], (err, row) => {
+      if (err || !row) {
+        res.status(404).send('Voucher no encontrado o ya canjeado');
+        return;
+      }
+  
+      // Marcar como canjeado
+      db.run('UPDATE vouchers SET redeemed = 1 WHERE id = ?', [id], function(err) {
+        if (err) {
+          res.status(500).send('Error al actualizar el estado del voucher');
+          return;
+        }
+        res.status(200).json({ message: row.message });
+      });
+    });
+  });
+  
---
+++ public/validate.js
@@ -0,0 +1,28 @@
+// Obtener el ID del voucher desde la URL
+const urlParams = new URLSearchParams(window.location.search);
+const voucherID = urlParams.get('voucher_id');
+
+// Función para validar el voucher
+async function validateVoucher() {
+  const response = await fetch(`http://localhost:3000/validate/${voucherID}`);
+
+  if (response.ok) {
+    const data = await response.json();
+    // Mostrar alerta de éxito con el mensaje del premio
+    Swal.fire(
+      '¡Válido!',
+      `Premio: ${data.message}`,
+      'success'
+    );
+  } else {
+    // Mostrar alerta de error indicando que el voucher ya ha sido canjeado o no existe
+    Swal.fire(
+      'Error',
+      'Este voucher ya ha sido canjeado o no es válido.',
+      'error'
+    );
+  }
+}
+
+// Ejecutar la validación al cargar la página
+validateVoucher();
